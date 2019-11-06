517a3cd2c5aa049cc8e98af44e5a28ab9baf3fa2
pszewach <paulaszewach@gmail.com>
2019-11-06 07:30:46 +0100

    Finished Lab

--- your-code/challenge-2.py
+++ your-code/challenge-2.py
@@ -1,38 +1,36 @@
-"""
-The code below generates a given number of random strings that consists of numbers and 
-lower case English letters. You can also define the range of the variable lengths of
-the strings being generated.
+import random
+import sys
+import string
 
-The code is functional but has a lot of room for improvement. Use what you have learned
-about simple and efficient code, refactor the code.
-"""
 
-def RandomStringGenerator(l=12, a=['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z','0','1','2','3','4','5','6','7','8','9']):
-    p = 0
+
+def RandomStringGenerator(l=12):
+    randomer=string.ascii_letters+string.digits
+    p=0
     s = ''
     while p<l:
-        import random
-        s += random.choice(a)
+        s += random.choice(randomer)
         p += 1
     return s
 
-def BatchStringGenerator(n, a=8, b=12):
+def BatchStringGenerator(n, min_len=8, max_len=12):
     r = []
     for i in range(n):
-        c = None
-        if a < b:
+        length = None
+        if min_len < max_len:
             import random
-            c = random.choice(range(a, b))
-        elif a == b:
-            c = a
+            length = random.choice(range(min_len, max_len))
+        elif min_len == max_len:
+            length = min_len
         else:
             import sys
             sys.exit('Incorrect min and max string lengths. Try again.')
-        r.append(RandomStringGenerator(c))
+        r.append(RandomStringGenerator(length))
     return r
 
-a = input('Enter minimum string length: ')
-b = input('Enter maximum string length: ')
-n = input('How many random strings to generate? ')
 
-print(BatchStringGenerator(int(n), int(a), int(b)))
+min_len = input('Enter minimum string length: ')
+max_len = input('Enter maximum string length: ')
+n_strings = input('How many random strings to generate? ')
+
+print(BatchStringGenerator(int(n_strings), int(min_len), int(max_len)))
--- your-code/challenge-3.py
+++ your-code/challenge-3.py
@@ -1,30 +1,17 @@
-"""
-You are presented with an integer number larger than 5. Your goal is to identify the longest side
-possible in a right triangle whose sides are not longer than the number you are given.
-
-For example, if you are given the number 15, there are 3 possibilities to compose right triangles:
-
-1. [3, 4, 5]
-2. [6, 8, 10]
-3. [5, 12, 13]
-
-The following function shows one way to solve the problem but the code is not ideal or efficient.
-Refactor the code based on what you have learned about code simplicity and efficiency.
-"""
-
-def my_function(X):
-    solutions = []
-    for x in range(5, X):
-        for y in range(4, X):
-            for z in range(3, X):
-                if (x*x==y*y+z*z):
-                    solutions.append([x, y, z])
-    m = 0
+def longest_side(max_len):
+    solutions = [[x,y,z]
+        for x in range(5, mx_len)
+        for y in range(4, max_len)
+        for z in range(3, max_len):
+        if (x*x==y*y+z*z)
+        ]
+    return max([max(solution) for solution in solutions]);
     for solution in solutions:
         if m < max(solution):
             m = max(solution)
     return m
 
-X = input("What is the maximal length of the triangle side? Enter a number: ")
+max_len = input("What is the maximal length of the triangle side? Enter a number: ")
 
-print("The longest side possible is " + str(my_function(int(X))))
+print("The longest side possible is " + str(longest_side(int(max_len))))
+
